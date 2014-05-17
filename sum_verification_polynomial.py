02798b9ffbce55fc10f9aabbb049a4d23278de13
Duncan Townsend <duncant@mit.edu>
2014-05-17 18:49:25 -0400

    Added script that I'm going to use to generate the algorithm for point sum verification

---
+++ sum_verification_polynomial.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "stupid_python_tricks"))
+
+from polynomial import *
+
+A = 486662
+#               c  Px Pz Qx Qy Qz Rx Rz
+numerator = [[   2, 3, 2, 2, 1, 1, 0, 1],
+             [ 2*A, 2, 3, 2, 1, 1, 0, 1],
+             [   2, 1, 4, 2, 1, 1, 0, 1],
+             [  -4, 4, 1, 1, 1, 2, 0, 1],
+             [-4*A, 3, 2, 1, 1, 2, 0, 1],
+             [  -4, 2, 3, 1, 1, 2, 0, 1],
+             [   2, 5, 0, 0, 1, 3, 0, 1],
+             [ 2*A, 4, 1, 0, 1, 3, 0, 1],
+             [   2, 3, 2, 0, 1, 3, 0, 1]]
+
+
+
+
+#                 c  Px Pz Qx Qy Qz Rx Rz
+denominator = [[  -1, 0, 5, 0, 0, 4, 1, 0],
+               [   1, 0, 5, 2, 2, 0, 0, 1],
+               [  -2, 1, 4, 1, 2, 1, 0, 1],
+               [   1, 3, 2, 2, 0, 2, 0, 1],
+               [   A, 2, 3, 2, 0, 2, 0, 1],
+               [   1, 1, 4, 2, 0, 2, 0, 1],
+               [   1, 2, 3, 0, 2, 2, 0, 1],
+               [  -2, 4, 1, 1, 0, 3, 0, 1],
+               [-2*A, 3, 2, 1, 0, 3, 0, 1],
+               [  -2, 2, 3, 1, 0, 3, 0, 1],
+               [  -1, 0, 5, 1, 0, 3, 0, 1],
+               [   1, 5, 0, 0, 0, 4, 0, 1],
+               [   A, 4, 1, 0, 0, 4, 0, 1],
+               [   1, 3, 2, 0, 0, 4, 0, 1],
+               [  -1, 1, 4, 0, 0, 4, 0, 1],
+               [  -A, 0, 5, 0, 0, 4, 0, 1]]
+
+vars = ['Px', 'Pz', 'Qx', 'Qy', 'Qz', 'Rx', 'Rz']
+
+n = Polynomial()
+for term_info in numerator:
+    term = Term(term_info[0])
+    for i, power in enumerate(term_info[1:]):
+        term *= Term((vars[i], power))
+    n += term
+
+d = Polynomial()
+for term_info in denominator:
+    term = Term(term_info[0])
+    for i, power in enumerate(term_info[1:]):
+        term *= Term((vars[i], power))
+    d += term
