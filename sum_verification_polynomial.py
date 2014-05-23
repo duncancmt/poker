9b09354973fc9b43becd08aaf0977a7bb2b0a867
Duncan Townsend <duncant@mit.edu>
2014-05-23 14:20:12 -0400

    Made sum_verification_polynomial.py importable. Made the numerator and denominator calculations run in parallel.

--- sum_verification_polynomial.py
+++ sum_verification_polynomial.py
@@ -3,9 +3,12 @@ import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), "stupid_python_tricks"))
 
 import cPickle as pickle
+from os import getpid
+from multiprocessing import Process
 
 from polynomial import *
 from horner import *
+from horner import _horner_verify
 from lru import *
 
 A = 486662
@@ -57,25 +60,51 @@ for term_info in denominator:
         term *= Term((vars[i], power))
     d += term
 
+numerator_pid = None
+denominator_pid = None
+
 def monitor(*args):
-    print args[0], args[1:]
+    if args[0] == "evaluate_tmp_alternatives":
+        pid = getpid()
+        if pid == numerator_pid:
+            print "numerator", args[2], args[3], args[4]
+        elif pid == denominator_pid:
+            print "denominator", args[2], args[3], args[4]
+        else:
+            raise RuntimeError("Unknown PID", pid)
 
-memo = LRUDict(maxsize=10000000)
-n_ops = horner_form_tmp(n, None, monitor, memo)
-f = open("sum_verification_polynomial_numerator.pkl","wb")
-p = pickle.Pickler(f, -1)
-p.dump(n_ops)
-del p
-f.flush()
-f.close()
-del f
+def compile_numerator(n_tmps):
+    global numerator_pid
+    numerator_pid = getpid()
+    memo = LRUDict(maxsize=1000000)
+    n_ops = horner_form_tmp(n, n_tmps, monitor, memo)
+    _horner_verify(n_ops, n)
+    f = open("sum_verification_polynomial_numerator.pkl","wb")
+    p = pickle.Pickler(f, -1)
+    p.dump(n_ops)
+    del p
+    f.flush()
+    f.close()
+    del f
 
-memo = LRUDidct(maxsize=10000000)
-d_ops = horner_form_tmp(d, None, monitor, memo)
-f = open("sum_verification_polynomial_denominator.pkl","wb")
-p = pickle.Pickler(f, -1)
-p.dump(d_ops)
-del p
-f.flush()
-f.close()
-del f
+def compile_denominator(n_tmps):
+    global denominator_pid
+    denominator_pid = getpid()
+    memo = LRUDict(maxsize=1000000)
+    d_ops = horner_form_tmp(d, n_tmps, monitor, memo)
+    _horner_verify(d_ops, d)
+    f = open("sum_verification_polynomial_denominator.pkl","wb")
+    p = pickle.Pickler(f, -1)
+    p.dump(d_ops)
+    del p
+    f.flush()
+    f.close()
+    del f
+
+if __name__ == '__main__':
+    num_p = Process(target=compile_numerator, kwargs={'n_tmps':None})
+    denom_p = Process(target=compile_denominator, kwargs={'n_tmps':None})
+    num_p.start()
+    denom_p.start()
+    num_p.join()
+    denom_p.join()
